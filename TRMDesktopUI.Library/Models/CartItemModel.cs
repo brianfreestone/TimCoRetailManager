27292902a29d755f98d24a3ad8f082741343538c
brianfreestone <briandfreestone@gmail.com>
2021-11-30 16:34:37 -0700

    Update Cart Quantities

--- TRMDesktopUI.Library/Models/CartItemModel.cs
+++ TRMDesktopUI.Library/Models/CartItemModel.cs
@@ -10,12 +10,12 @@ namespace TRMDesktopUI.Library.Models
     {
         public ProductModel Product { get; set; }
         public int QuantityInCart { get; set; }
-        public string DisplayText
-        {
-            get
-            {
-                return $"{ Product.ProductName} ({QuantityInCart})";
-            }
-        }
+        //public string DisplayText
+        //{
+        //    get
+        //    {
+        //        return $"{ Product.ProductName} ({QuantityInCart})";
+        //    }
+        //}
     }
 }
